d4e5479823d50f94553c9e14a24d5e17a5767e3d
i-saint <i-saint@328a3827-565c-1344-a4b1-adef33bfefbb>
2012-01-15 12:57:42 +0000

    float16
    git-svn-id: file:///c/svn/atomic@147 328a3827-565c-1344-a4b1-adef33bfefbb

--- types.h
+++ types.h
@@ -13,6 +13,7 @@ typedef unsigned char       uint8;
 typedef unsigned short      uint16;
 typedef unsigned int        uint32;
 typedef unsigned long long  uint64;
+typedef half                float16;
 typedef float               float32;
 typedef double              float64;
 
