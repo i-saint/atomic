4013beb327533df95ba67f657febb8a32768bea1
i-saint <saint.skr@gmail.com>
2013-01-30 21:08:06 +0900

    あげ忘れ

---
+++ ist/Concurrency/Timer.cpp
@@ -0,0 +1,30 @@
+﻿#include "istPCH.h"
+#include "Timer.h"
+
+namespace ist {
+
+#ifdef ist_env_Windows
+
+Timer::Timer()
+{
+    ::QueryPerformanceFrequency( &m_freq );
+    reset();
+}
+
+void Timer::reset()
+{
+    m_start.QuadPart = 0;
+    ::QueryPerformanceCounter( &m_start );
+}
+
+float32 Timer::getElapsedMillisec() const
+{
+    LARGE_INTEGER end;
+    ::QueryPerformanceCounter( &end );
+    return ((float32)(end.QuadPart - m_start.QuadPart) / (float32)m_freq.QuadPart)*1000.0f;
+}
+#else // todo
+
+#endif // 
+
+} // namespace ist
---
+++ ist/Concurrency/Timer.h
@@ -0,0 +1,26 @@
+﻿#ifndef ist_Concurrency_Timer_h
+#define ist_Concurrency_Timer_h
+
+#include "ist/Config.h"
+
+namespace ist {
+
+class Timer
+{
+public:
+    Timer();
+    void reset();
+    float32 getElapsedMillisec() const;
+
+private:
+#ifdef ist_env_Windows
+    LARGE_INTEGER m_freq;
+    LARGE_INTEGER m_start;
+#else // todo
+#endif // 
+
+};
+
+} // namespace ist
+
+#endif // ist_Concurrency_Timer_h
