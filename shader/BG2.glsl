f016117c976624e2f1d8b01407e8002f92455725
i-saint <saint.skr@gmail.com>
2013-06-11 22:48:09 +0900

    bg

--- shader/BG2.glsl
+++ shader/BG2.glsl
@@ -98,48 +98,42 @@ void main()
         total_d += d;
         ray += rayDir * d;
         if(d<0.001) {break; }
-        if(total_d>MAX_DISTANCE) {
-            total_d = MAX_DISTANCE;
-            march = MAX_MARCH;
-            ray = camPos + rayDir*MAX_DISTANCE;
-            break;
-        }
     }
+
 
     float glow = 0.0;
     {
         const float s = 0.01;
         vec3 p = ray;
-        if(total_d>MAX_DISTANCE) {
-        }
-        else {
-            vec3 n1 = genNormal(ray);
-            vec3 n2 = genNormal(ray+vec3(s, 0.0, 0.0));
-            vec3 n3 = genNormal(ray+vec3(0.0, s, 0.0));
-            if(dot(n1, n2)<0.9 || dot(n1, n3)<0.9) {
-                glow = 0.3;
-            }
+        vec3 n1 = genNormal(ray);
+        vec3 n2 = genNormal(ray+vec3(s, 0.0, 0.0));
+        vec3 n3 = genNormal(ray+vec3(0.0, s, 0.0));
+        glow = max(1.0-abs(dot(camDir, n1)-0.5), 0.0)*0.5;
+        if(dot(n1, n2)<0.8 || dot(n1, n3)<0.8) {
+            glow += 0.4;
         }
     }
     {
         vec3 p = rotz * ray;
         p = roty * p;
-        float grid1 = max(glow, max((mod((p.x+p.y+p.z*2.0)-time*2.5, 5.0)-4.0)*1.5, 0.0) );
-        float grid2 = max(glow, max((mod((p.x+p.y*2.0+p.z)-time*2.0, 7.0)-6.0)*1.2, 0.0) );
+        float grid1 = max((mod((p.x+p.y+p.z*2.0)-time*3.0, 5.0)-4.0)*1.5, 0.0);
+        float grid2 = max((mod((p.x+p.y*2.0+p.z)-time*2.0, 7.0)-6.0)*1.2, 0.0);
         vec3 gp1 = abs(mod(p, vec3(0.29)));
         vec3 gp2 = abs(mod(p, vec3(0.36)));
         if(gp1.x<0.28 && gp1.y<0.28) {
             grid1 = 0.0;
         }
-        if(gp2.x<0.345 && gp2.y<0.345) {
+        if(gp2.y<0.345 && gp2.z<0.345) {
             grid2 = 0.0;
         }
         glow += grid1+grid2;
     }
 
-    float fog = min(1.0, (1.0 / float(MAX_MARCH-4)) * march);
-    glow *= min(1.0, 4.0-(4.0 / float(MAX_MARCH)) * march);
-    ps_FlagColor = vec4(vec3(0.2+glow*0.75, 0.2+glow*0.75, 0.3+glow)*fog, 1.0);
+
+    float fog = min(1.0, (1.0 / float(MAX_MARCH)) * float(march));
+    vec3  fog2 = 0.01 * vec3(1, 1, 1.5) * total_d;
+    glow *= min(1.0, 4.0-(4.0 / float(MAX_MARCH-1)) * float(march));
+    ps_FlagColor = vec4(vec3(0.15+glow*0.75, 0.15+glow*0.75, 0.2+glow)*fog + fog2, 1.0);
 }
 
 #endif
