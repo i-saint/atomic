92498299114c1792eba7757a3ff6457d27dfc18a
i-saint <saint.skr@gmail.com>
2013-09-21 02:33:48 +0900

    あげ忘れ

---
+++ ist/Base/StringUtil.cpp
@@ -0,0 +1,61 @@
+﻿#include "istPCH.h"
+#include "StringUtil.h"
+
+namespace ist {
+
+istAPI size_t scan(const std::string &content, const std::regex &pattern, const std::function<void (const std::cmatch &m)> &f)
+{
+    std::cmatch m;
+    size_t num = 0;
+    size_t pos = 0;
+    while(std::regex_search(content.c_str()+pos, m, pattern)) {
+        f(m);
+        pos += m.position()+m.length();
+        ++num;
+    }
+    return num;
+}
+
+istAPI std::string sub(const std::string &content, const std::regex &pattern, const std::function<std::string (const std::cmatch &m)> &f)
+{
+    struct replace_info {
+        size_t pos, len;
+        std::string replacement;
+    };
+    std::vector<replace_info> replacements;
+    std::cmatch m;
+    if(std::regex_search(content.c_str(), m, pattern)) {
+        replace_info ri = {m.position(), m.length(), f(m)};
+        replacements.push_back(ri);
+    }
+
+    std::string ret = content;
+    std::for_each(replacements.rbegin(), replacements.rend(), [&](replace_info &ri){
+        ret.replace(ri.pos, ri.len, ri.replacement);
+    });
+    return ret;
+}
+
+istAPI std::string gsub(const std::string &content, const std::regex &pattern, const std::function<std::string (const std::cmatch &m)> &f)
+{
+    struct replace_info {
+        size_t pos, len;
+        std::string replacement;
+    };
+    std::vector<replace_info> replacements;
+    std::cmatch m;
+    size_t pos = 0;
+    while(std::regex_search(content.c_str()+pos, m, pattern)) {
+        replace_info ri = {pos+m.position(), m.length(), f(m)};
+        replacements.push_back(ri);
+        pos += m.position()+m.length();
+    }
+
+    std::string ret = content;
+    std::for_each(replacements.rbegin(), replacements.rend(), [&](replace_info &ri){
+        ret.replace(ri.pos, ri.len, ri.replacement);
+    });
+    return ret;
+}
+
+} // namespace ist
---
+++ ist/Base/StringUtil.h
@@ -0,0 +1,15 @@
+﻿#ifndef ist_Base_StringUtil_h
+#define ist_Base_StringUtil_h
+#include "ist/Config.h"
+#include <string>
+#include <regex>
+#include <functional>
+
+namespace ist {
+
+istAPI size_t scan(const std::string &content, const std::regex &pattern, const std::function<void (const std::cmatch &m)> &f);
+istAPI std::string sub(const std::string &content, const std::regex &pattern, const std::function<std::string (const std::cmatch &m)> &f);
+istAPI std::string gsub(const std::string &content, const std::regex &pattern, const std::function<std::string (const std::cmatch &m)> &f);
+
+} // namespace ist
+#endif // ist_Base_StringUtil_h
